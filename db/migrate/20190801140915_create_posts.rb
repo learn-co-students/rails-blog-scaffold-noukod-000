d169d454d3daa2da1e0c8b7cc767176b4bb56b71
Herna Michel <labellehernamichel@yahoo.fr>
2019-08-01 10:12:14 -0400

    Done.

--- db/migrate/20190801140915_create_posts.rb
+++ db/migrate/20190801140915_create_posts.rb
@@ -1,6 +1,6 @@
-class CreateApartments < ActiveRecord::Migration[5.0]
+class CreatePosts < ActiveRecord::Migration[5.0]
   def change
-    create_table :apartments do |t|
+    create_table :posts do |t|
       t.string :address
       t.float :price
       t.text :description
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190801135754) do
+ActiveRecord::Schema.define(version: 20190801140915) do
 
   create_table "apartments", force: :cascade do |t|
+    t.string   "address"
+    t.float    "price"
+    t.text     "description"
+    t.string   "image_url"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
     t.string   "address"
     t.float    "price"
     t.text     "description"
