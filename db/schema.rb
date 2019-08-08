a3c5df768e32517737c9a6d0bf3a0d1fc4c38bd2
Andy Feidje Joseph <ingandy75@gmail.com>
2019-08-08 16:06:00 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190708041746) do
+ActiveRecord::Schema.define(version: 20190808160016) do
+
+  create_table "apartments", force: :cascade do |t|
+    t.string   "address"
+    t.float    "price"
+    t.text     "description"
+    t.string   "image_url"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
